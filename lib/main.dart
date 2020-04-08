983f38395d3fa85e20a602f4a8b768c7649e5b3c
sergghazaryan <serg.ghazaryan@mail.ru>
2020-04-08 11:53:22 +0300

    add to giyhub

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './screens/products_overview.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
